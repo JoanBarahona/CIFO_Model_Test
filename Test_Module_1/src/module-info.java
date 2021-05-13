5131d660f3a844558bc28e1107aaab99bebd2e04
mati <mati@DESKTOP-9VEAPQP>
2021-05-13 12:43:46 +0200

    prueba_model_1_method

---
+++ Test_Module_1/src/module-info.java
@@ -0,0 +1,2 @@
+module test_Module_1 {
+}
---
+++ Test_Module_1/src/module_test1/Init.java
@@ -0,0 +1,11 @@
+package module_test1;
+
+public class Init {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		TestPerson.executePerson();
+	}
+
+}
